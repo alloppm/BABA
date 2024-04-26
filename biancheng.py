b59918e371f3743eeea8ce77f78e16693a49acc3
alloppm <eaavince@gmail.com>
2024-04-26 15:46:05 +0800

    Update biancheng.py

--- biancheng.py
+++ biancheng.py
@@ -508,6 +508,6 @@ dd = lambda : getOne("dd")
 av = lambda : getOne("av")
 ss_ssv = lambda : getOne("ss_ssv")
 ex_ss_ssv = lambda : getOne("ex_ss_ssv")
-bb = lambda : getOne("bb_bbc_bbv")
+bb = lambda : getOne("bb")
 
 all = lambda : getOne("all")
